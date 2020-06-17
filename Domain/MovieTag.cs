8fd3586b20c94e8c5e46fe533b528a1ecc2ab28d
Joris Verhelst <joris.verhelst@v8897@student.hogent.be>
2020-06-17 15:58:44 +0200

    working on MovieTag

---
+++ Domain/MovieTag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWeb.Domain
+{
+    public class MovieTag
+    {
+        public Movie Movie { get; set; }
+        public int MovieID { get; set; }
+        public Tag Tag { get; set; }
+        public int TagID { get; set; }
+    }
+}
